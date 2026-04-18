b481e0661ab7758937fcdec62e84432226fe0c07
WerideMarcondes <weride.welide@hotmail.com>
2026-04-17 21:50:55 -0400

    add hand alert

--- js/index.js
+++ js/index.js
@@ -15,10 +15,22 @@ function nextPicture() {
 }
 
 function logo() {
-   Swal.fire({
-      title: "You will be Pumped",
-      text: "",
-      icon: "success",
-    });
+  //  Swal.fire({
+  //     title: "You will be Pumped",
+  //     text: "",
+  //     icon: "success",
+  //   });
+
+    Swal.fire({
+  title: "You've been Pumped",
+  imageUrl: './img/backgroud/hand.jpeg', // Caminho src local
+  width: '320px', // Define a largura aqui
+  height: '100',
+  imageWidth: 140,
+  imageHeight: 200,
+  color: 'black',
+  confirmButtonColor: 'rgb(243, 105, 55)',
+  confirmButtonColor: 'black'
+});
 
 }
